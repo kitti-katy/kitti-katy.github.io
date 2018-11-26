19477ffea6226f29604b57992722f6cf131c0f64
Yekaterina Ankudinova <kitti-katy@mail.ru>
2018-11-25 22:14:27 -0500

    initial commit

---
+++ precache-manifest.008aaf2fce7e5dc3a9a87bf044facda4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9089d613cf2ea1a13a9f",
+    "url": "/static/js/main.9089d613.chunk.js"
+  },
+  {
+    "revision": "a9b5ace3eb0d45513b30",
+    "url": "/static/js/1.a9b5ace3.chunk.js"
+  },
+  {
+    "revision": "9089d613cf2ea1a13a9f",
+    "url": "/static/css/main.bf02a67b.chunk.css"
+  },
+  {
+    "revision": "a9b5ace3eb0d45513b30",
+    "url": "/static/css/1.05eb3164.chunk.css"
+  },
+  {
+    "revision": "801c6ce9b953727927dcd6fdeabb47b0",
+    "url": "/index.html"
+  }
+];
