734d9890b9e660da4f5f0fdb2b1023117f99c3bb
Yekaterina Ankudinova <kitti-katy@mail.ru>
2019-01-13 22:00:46 -0500

    v 0.3

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7374c8b89b18afc23cbea37c82542e3c.js"
+  "/precache-manifest.c4e1e05265960fd2650df6627c78ff18.js"
 );
 
 workbox.clientsClaim();
