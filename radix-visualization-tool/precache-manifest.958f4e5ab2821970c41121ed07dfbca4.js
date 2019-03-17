65eb2d16f77bf313eb61023171727ad34c0b50c4
Yekaterina Ankudinova <kitti-katy@mail.ru>
2019-03-17 16:49:32 -0400

    added version 2 for cpg and radix tool

---
+++ radix-visualization-tool/precache-manifest.958f4e5ab2821970c41121ed07dfbca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "6656f56ce2b4f7651077",
+    "url": "/static/js/main.c5a5aa5d.chunk.js"
+  },
+  {
+    "revision": "d14408cb011c7113242e",
+    "url": "/static/js/2.4b8ce697.chunk.js"
+  },
+  {
+    "revision": "6656f56ce2b4f7651077",
+    "url": "/static/css/main.3a3fc977.chunk.css"
+  },
+  {
+    "revision": "fc6b6828b6e6304b8edf6c2bf0da52aa",
+    "url": "/index.html"
+  }
+];
