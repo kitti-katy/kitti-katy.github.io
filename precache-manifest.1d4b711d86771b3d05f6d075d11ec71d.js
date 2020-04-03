a22394097ee60426f5a374472c4d5f06d1c1e4d7
Yekaterina Ankudinova <kitti-katy@mail.ru>
2020-04-03 14:26:18 -0400

    now it is a typing tutor

---
+++ precache-manifest.1d4b711d86771b3d05f6d075d11ec71d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb68165a08ed2baa6bc77d5c57398332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24325d31d574266ddea7",
+    "url": "/static/css/main.ae584bdd.chunk.css"
+  },
+  {
+    "revision": "2c73b0728871f8751aef",
+    "url": "/static/js/2.ec7ae52b.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.ec7ae52b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24325d31d574266ddea7",
+    "url": "/static/js/main.6ce2fe49.chunk.js"
+  },
+  {
+    "revision": "a8b53038bcbddd115cc9",
+    "url": "/static/js/runtime-main.ae743c76.js"
+  }
+]);
